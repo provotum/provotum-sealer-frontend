244ac5b8215ea3a99d04b2939b350074dba15929
Claudio Brasser <brasser13@gmail.com>
2021-02-26 16:11:02 +0100

    fixed a naming bug

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import './App.css';
-import { selectSealers, triggerSendAddress, loadSealers, loadChainSpecForSealer, startChainNodeForSealer, insertValidatorKeysForSealer, selectVotingAuthority, getElections, selectElections, triggerDkgForElection, tally } from './features/chain/chainSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import React, { useEffect } from 'react'
+import "./App.css";
+import {
+  selectSealers,
+  triggerSendAddress,
+  loadSealers,
+  loadChainSpecForSealer,
+  startChainNodeForSealer,
+  insertValidatorKeysForSealer,
+  selectVotingAuthority,
+  getElections,
+  selectElections,
+  triggerDkgForElection,
+  tally,
+} from "./features/chain/chainSlice";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from "react";
 
 function App() {
-
   const dispatch = useDispatch();
   const sealers = useSelector(selectSealers);
   //const votingAuthority = useSelector(selectVotingAuthority);
-  const votingAuthority = process.env.REACT_APP_VA_URL
+  const votingAuthority = process.env.REACT_APP_VA_URL;
   const elections = useSelector(selectElections);
   const dispatchSendAddress = (sealer) => {
     dispatch(triggerSendAddress(sealer));
-  }
+  };
   const dispatchGetChainSpec = (sealer) => {
     dispatch(loadChainSpecForSealer(sealer));
-  }
+  };
   const dispatchStartChainNode = (sealer) => {
     dispatch(startChainNodeForSealer(sealer));
-  }
+  };
   const dispatchValidatorKeys = (sealer) => {
     dispatch(insertValidatorKeysForSealer(sealer));
-  }
+  };
 
   const dipatchTally = (sealer, electionId) => {
     dispatch(tally({ sealer: sealer, electionId: electionId }));
-  }
+  };
   const dispatchDkg = (sealer, electionId) => {
     console.log(sealer.url, electionId);
     dispatch(triggerDkgForElection({ sealer: sealer, electionId: electionId }));
-  }
+  };
 
   useEffect(() => {
     dispatch(loadSealers());
@@ -40,60 +51,111 @@ function App() {
   }, [dispatch, votingAuthority]);
 
   const getMapValues = (spec) => {
-    let list = Object.entries(spec).map(s => {
-      return { key: s[0], value: String(s[1]) }
+    let list = Object.entries(spec).map((s) => {
+      return { key: s[0], value: String(s[1]) };
     });
     return list;
-  }
+  };
 
-  const renderSpec = (sealer) => getMapValues(sealer.spec).map(t => (
-    <div key={t.key} className="spec">
-      <div><p variant="body1">{t.key}: {t.value}</p></div>
-    </div>
-  ));
-  const renderChain = (sealer) => getMapValues(sealer.chain).map(t => (
-    <div key={t.key} className="spec">
-      <div><p variant="body1">{t.key}: {t.value}</p></div>
-    </div>
-  ));
+  const renderSpec = (sealer) =>
+    getMapValues(sealer.spec).map((t) => (
+      <div key={t.key} className="spec">
+        <div>
+          <p variant="body1">
+            {t.key}: {t.value}
+          </p>
+        </div>
+      </div>
+    ));
+  const renderChain = (sealer) =>
+    getMapValues(sealer.chain).map((t) => (
+      <div key={t.key} className="spec">
+        <div>
+          <p variant="body1">
+            {t.key}: {t.value}
+          </p>
+        </div>
+      </div>
+    ));
 
-  const sendWalletButtons = () => sealers.map(s => (
-    <div>
-      <h1>Sealer</h1>
-      <p>name: {s.name}</p>
-      <p>address: {s.url}</p>
-      <h2>Status</h2>
+  const sendWalletButtons = () =>
+    sealers.map((s) => (
+      <div className="sealer">
+        <div className="section">
+          <div className="section-title">Sealer</div>
+          <div className="section-content">
+            <p>name: {s.name}</p>
+            <p>address: {s.url}</p>
+            <h2>Status</h2>
+          </div>
+        </div>
+        <div className="section">
+          <div className="section-title">Bootstrap</div>
+          <div className="section-content">
+            <button
+              onClick={() => {
+                dispatchSendAddress(s);
+              }}
+            >
+              send adress to VA
+            </button>
+            <button
+              onClick={() => {
+                dispatchGetChainSpec(s);
+              }}
+            >
+              get chain spec
+            </button>
+            <button
+              onClick={() => {
+                dispatchStartChainNode(s);
+              }}
+            >
+              start chain node
+            </button>
+            <button
+              onClick={() => {
+                dispatchValidatorKeys(s);
+              }}
+            >
+              insert validator keys
+            </button>
+          </div>
+        </div>
+        <div className="section"></div>
+        <h2>spec</h2>
+        {renderSpec(s)}
+        <h2>chain</h2>
+        {renderChain(s)}
+        <h2>actions</h2>
+        <div className="elections">{renderElections(s)}</div>
+      </div>
+    ));
 
-      <h2>spec</h2>
-      {renderSpec(s)}
-      <h2>chain</h2>
-      {renderChain(s)}
-      <h2>actions</h2>
-      <button onClick={() => { dispatchSendAddress(s) }}>send adress to VA</button>
-      <button onClick={() => { dispatchGetChainSpec(s) }}>get chain spec</button>
-      <button onClick={() => { dispatchStartChainNode(s) }}>start chain node</button>
-      <button onClick={() => { dispatchValidatorKeys(s) }}>insert validator keys</button>
-      {renderElections(s)}
-    </div>
-  ));
+  const renderElections = (sealer) =>
+    elections.map((e) => (
+      <div className="election" key={e.electionId}>
+        <h3>{e.title}</h3>
+        <p>{e.electionId}</p>
+        <p>phase: {e.phase}</p>
+        <button
+          onClick={() => {
+            dispatchDkg(sealer, e.electionId);
+          }}
+        >
+          create and store public key on the bc
+        </button>
+        <button
+          onClick={() => {
+            dipatchTally(sealer, e.electionId);
+          }}
+        >
+          tally
+        </button>
+      </div>
+    ));
 
-  const renderElections = (sealer) => elections.map(e => (
-    <div key={e.electionId}>
-      <h3>{e.title}</h3>
-      <p>{e.electionId}</p>
-      <p>phase: {e.phase}</p>
-      <button onClick={() => { dispatchDkg(sealer, e.electionId) }}>create and store public key on the bc</button>
-      <button onClick={() => { dipatchTally(sealer, e.electionId) }}>tally</button>
-
-    </div>
-  ));
-
-  return (
-    <div className="App">
-      {sendWalletButtons()}
-
-    </div>
-  );
+  return <div className="app">{sendWalletButtons()}</div>;
 }
 
 export default App;
--- src/features/chain/api/index.js
+++ src/features/chain/api/index.js
@@ -1,125 +1,126 @@
-
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 export const distributedKeyGeneration = (sealerUrl) => {
-    console.log('starting DKG!')
-    axios.post(`${sealerUrl}/dkg`);
-}
+  console.log("starting DKG!");
+  axios.post(`${sealerUrl}/dkg`);
+};
 
 export const sendAddress = (sealer) => {
-    console.log(sealer.url);
-    axios.post(`${sealer.url}/wallet`)
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .then(function () {
-            // always executed
-            console.log('done');
-        });
-
-}
+  console.log(sealer.url);
+  axios
+    .post(`${sealer.url}/wallet`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+      console.log("done");
+    });
+};
 
 export const getSealers = async () => {
-    return [
-        {
-            url: process.env.REACT_APP_SEALER_1_URL,
-            name: 'bob',
-            spec: {},
-            chain: {},
-            keys: {},
-            chainNodeRunning: false,
-            specLoaded: false,
-            backendOnline: false,
-        },
-        {
-            url: process.env.REACT_APP_SEALER_2_URL,
-            name: 'max',
-            spec: {},
-            chain: {},
-            keys: {},
-            chainNodeRunning: false,
-            specLoaded: false,
-            backendOnline: false,
-        }
-    ]
-}
+  return [
+    {
+      url: process.env.REACT_APP_SEALER_1_URL,
+      name: "bob",
+      spec: {},
+      chain: {},
+      keys: {},
+      chainNodeRunning: false,
+      specLoaded: false,
+      backendOnline: false,
+    },
+    {
+      url: process.env.REACT_APP_SEALER_2_URL,
+      name: "max",
+      spec: {},
+      chain: {},
+      keys: {},
+      chainNodeRunning: false,
+      specLoaded: false,
+      backendOnline: false,
+    },
+  ];
+};
 
 export const getVaUrl = async () => {
-    return process.env.REACT_APP_VA_URL
-}
+  return process.env.REACT_APP_VA_URL;
+};
 
 export const getVotes = async (vaUrl) => {
-    console.log('fetching votes');
-    try {
-        let response = await axios.get(`${vaUrl}/votes`);
-        console.log(response);
-        return response.data;
-    } catch (e) {
-        console.log(e);
-        return []
-    }
-}
+  console.log("fetching votes");
+  try {
+    let response = await axios.get(`${vaUrl}/votes`);
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
 
 export const triggerDkg = async (sealer, electionId) => {
-    console.log('doing dkg for election: ', electionId);
-    try {
-        let response = await axios.post(`${sealer.url}/dkg`, { voteId: electionId });
-        console.log(response);
-    } catch (e) {
-        console.log(e);
-    }
-}
+  console.log("doing dkg for election: ", electionId);
+  try {
+    let response = await axios.post(`${sealer.url}/dkg`, {
+      voteId: electionId,
+    });
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const fetchChainSpec = async (sealer) => {
-    let response = await axios.post(`${sealer.url}/bootstrap/chain-spec`);
-    return {
-        sealerName: sealer.name,
-        response: response.data,
-    };
-}
+  let response = await axios.post(`${sealer.url}/bootstrap/chain-spec`);
+  return {
+    sealerName: sealer.name,
+    response: response.data,
+  };
+};
 
 export const startChainNode = async (sealer) => {
-    try {
-        let response = await axios.post(`${sealer.url}/bootstrap/chain/bootnode?restart=false`);
-        console.log(response)
-        return {
-            sealerName: sealer.name,
-            response: response.data,
-        }
-    } catch (e) {
-        console.log(e)
-    }
-
-}
+  try {
+    let response = await axios.post(
+      `${sealer.url}/bootstrap/chain/bootnode?restart=false`
+    );
+    console.log(response);
+    return {
+      sealerName: sealer.name,
+      response: response.data,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const insertValidatorKeys = async (sealer) => {
-    console.log('inserting validator keys')
-    let response = await axios.post(`${sealer.url}/bootstrap/validators`);
-    console.log(response.data)
-    return {
-        sealerName: sealer.name,
-        response: response.data,
-    }
-}
+  console.log("inserting validator keys");
+  let response = await axios.post(`${sealer.url}/bootstrap/validators`);
+  console.log(response.data);
+  return {
+    sealerName: sealer.name,
+    response: response.data,
+  };
+};
 
 export const startTally = async (sealer, electionId) => {
-    console.log('starting tally');
-    try {
-        let response = await axios.post(`${sealer.url}/tally`, { voteId: electionId });
-        console.log(response.data)
-        return {
-            sealerName: sealer.name,
-            response: response.data,
-        }
-    } catch (e) {
-        console.log(e)
-    }
-
-}
-
-
+  console.log("starting tally");
+  try {
+    let response = await axios.post(`${sealer.url}/tally`, {
+      voteId: electionId,
+    });
+    console.log(response.data);
+    return {
+      sealerName: sealer.name,
+      response: response.data,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
